bd7e1b0ea813ca9cee3002feaae5150f8c7d09be
Morgan Carter <morgancarter1@gmail.com>
2017-03-12 14:47:22 +1100

    Add in instant click and Crisp chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,34 @@
-/* Make CSS :active work on mobile */
-document.addEventListener("touchstart", function() {},false);
 
 
 
-/* Drift Live Chat */
-!function() {
-	var t;
-	if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
-	t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-	t.factory = function(e) {
-		return function() {
-			var n;
-			return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
-		};
-	}, t.methods.forEach(function(e) {
-		t[e] = t.factory(e);
-	}), t.load = function(t) {
-		var e, n, o, i;
-		e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"), 
-		o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
-		n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
-	});
-}();
-drift.SNIPPET_VERSION = '0.3.1';
-drift.load('t3xvtekkcwmi');
 
-drift.on('ready', function(api) {
-	api.widget.hide()
-    $('.js-openChat').click(function() {
-        api.sidebar.open();
-    });
+/*******************
+ * Crisp Live Chat *
+ *******************/
+$crisp = [];
+CRISP_WEBSITE_ID = '2c543135-0dc6-4c8a-9f3b-95372300a407';
+(function(){d=document;s=d.createElement('script');s.src='https://client.crisp.im/l.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})();
+
+$('.js-openChat').attr('disabled', true);
+$crisp.push(["on", "session:loaded", function() {
+	// $crisp.do("chat:hide");
+	$('.js-openChat').attr('disabled', false);
+}]);
+$crisp.push(["on", "chat:closed", function() {
+	// $crisp.do("chat:hide");
+}]);
+
+$(document).on('click', '.js-openChat', function() {
+	console.log('q')
+    $crisp.do("chat:show");
+    $crisp.do("chat:open");
 });
 
 
 
-/* Google Analytics */
+/********************
+ * Google Analytics *
+ ********************/
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -43,17 +37,45 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-63372069-4', 'auto');
 ga('send', 'pageview');
 
+// Send an event on page exit, so that time is tracked properly for bounce pages.
+var numPagesVisited = 0;
+window.onbeforeunload = function(){
+	ga('send', 'event', 'time-tracking', 'page-exit');
+	if (numPagesVisited === 1)
+		ga('send', 'event', 'engagement', 'page-bounce');
+}
 
 
-/* Checkout Page */
+
+
+/*****************
+ * Instant Click *
+ *****************/
+InstantClick.on('change', function() {
+	ga('send', 'pageview', location.pathname + location.search);
+	numPagesVisited++;
+	// initExternalLinks();
+});
+
+InstantClick.init();
+
+
+
+/****************
+ * Interactions *
+ ***************/
+// Make CSS :active work on mobile
+document.addEventListener("touchstart", function() {},false);
+
+
+// Update Pricing with Extras
 $('.js-serviceCheckout-option').change(function() {
 	var totalPrice = $('.js-serviceCheckout-price');
 	totalPrice.text(parseInt(totalPrice.text()) + $(this).data('price'));
 });
 
 
-
-/* Submit Form */
+// Submit Form
 $(document).on('click', '.js-serviceCheckout-submit', function(e){
 	e.preventDefault(); // prevent the form to do the post.
 	$(this).addClass('loading');
